5141b3b4b01e45a6a5fb14ea82f00c23f4a43c6d
Anja Plemenitas <anja.plemenitas@gmail.com>
2022-04-21 14:46:10 +0100

    lines added

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -4,7 +4,17 @@ import "./contact.css";
 
 export const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className="contact">
+      <hr className="main-line"></hr>
+        <div>Contact</div>
+      <hr className="small-line"></hr>
+        <div>Socials</div>
+      <ul>
+        <li>Linkedin</li>
+        <li>GitHub</li>
+        <li>Instagram</li>
+      </ul>
+    </div>
   )
 }
 
