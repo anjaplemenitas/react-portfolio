0c98ac37363da15e42a64fbf9687626e6143ce5d
Anja Plemenitas <anja.plemenitas@gmail.com>
2022-04-21 11:29:25 +0100

    added react-icon package

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import "../app.css";
+import { FaGithub } from "react-icons/fa";
+
+class Question extends React.Component {
+  render() {
+    return <h3> Lets go for a <FaGithub />? </h3>
+  }
+}
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <div>
+      <div>Designed & Built by Anja Plemenitas</div>
+      <div>  • React  • CSS  • Html  • JavaScript  </div>
+      <Question />
+    </div>
   )
 }
 
