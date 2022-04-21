f341c28cdd130e27fed3ba9679af8bafe1651b3b
Anja Plemenitas <anja.plemenitas@gmail.com>
2022-04-21 12:17:47 +0100

    fotter added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,16 +4,18 @@ import { FaGithub } from "react-icons/fa";
 
 class Question extends React.Component {
   render() {
-    return <h3> Lets go for a <FaGithub />? </h3>
+    return <FaGithub />
   }
 }
 
 export const Footer = () => {
   return (
-    <div>
-      <div>Designed & Built by Anja Plemenitas</div>
-      <div>  • React  • CSS  • Html  • JavaScript  </div>
-      <Question />
+    <div className="footer">
+      <div className="built-with-footer">
+        <div>• React  • CSS  • Html  • JavaScript</div>
+        <a href="/"><Question /></a>
+      </div>
+      <div className="title-footer">Designed & Built by Anja Plemenitas</div>
     </div>
   )
 }
