9b38c1c5ece7a94161a537fd80db85e8ce981711
Anja Plemenitas <anja.plemenitas@gmail.com>
2022-04-22 11:57:13 +0100

    projects added in list

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,9 +1,49 @@
 import React from 'react';
 import "./projects.css";
+import data from './portfolio.js';
+import { FaGithub } from "react-icons/fa";
+
+// logo
+class Question extends React.Component {
+  render() {
+    return <FaGithub />
+  }
+}
 
 function Projects() {
+  // let data = ["Ram", "Shyam", "Sita", "Gita"];
+  // let list = document.getElementById("myList");
+
   return (
-    <div>Projects</div>
+    <div>
+      <div>Projects</div>
+      <hr className="main-line"></hr>
+      {
+        data.map(({id, title, type, made_with, github}) => {
+          return (
+            <section className="project-container">
+              <div className="project-img">
+                <img scr="/" alt="/"></img>
+              </div>
+              <div className="project-info">
+                <div className="project-title">
+                  <h3>{title}</h3>
+                  <h4>{type}</h4>
+                </div>
+                <div className="project-links">
+                  <ul className="project-made-with">
+                  { made_with.map((item) =>
+                    <li>{item}</li>
+                  )}
+                  </ul>
+                  <a href={github}><Question /></a>
+                </div>
+              </div>
+            </section>
+          )
+          })
+        }
+    </div>
   )
 }
 
---
+++ src/components/projects/portfolio.js
@@ -0,0 +1,21 @@
+const data = [
+  {
+    id: 1,
+    // image: ,
+    title: 'OddRecipe',
+    type: 'WebApp',
+    made_with: ['Rails', 'Ruby', 'HTML', 'Css', 'JavaScript'],
+    github: 'https://github.com/anjaplemenitas/OddRecipe/blob/master/README.md',
+  },
+
+  {
+    id: 2,
+    image: 'https://user-images.githubusercontent.com/93189774/162631183-06116356-0a5d-4ca9-8a1c-de48a5a82a07.png',
+    title: 'Games in the Dungeon',
+    type: 'Web',
+    made_with: ['Rails', 'Ruby', 'HTML', 'Css'],
+    github: 'https://github.com/anjaplemenitas/games_in_the_dungeon/blob/master/README.md',
+  }
+]
+
+export default data;
