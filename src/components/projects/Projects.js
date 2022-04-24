017858904702fefb0ce21a4aaeda29210e1381a0
Anja Plemenitas <anja.plemenitas@gmail.com>
2022-04-24 16:41:45 +0100

    cards are responsive

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -3,6 +3,7 @@ import "./projects.css";
 import data from './portfolio.js';
 import { FaGithub } from "react-icons/fa";
 
+
 // logo
 class Question extends React.Component {
   render() {
@@ -11,38 +12,37 @@ class Question extends React.Component {
 }
 
 function Projects() {
-  // let data = ["Ram", "Shyam", "Sita", "Gita"];
-  // let list = document.getElementById("myList");
-
   return (
-    <div>
-      <div>Projects</div>
+    <div className="project">
+      <h3>Projects</h3>
       <hr className="main-line"></hr>
+      <section className="project-container">
       {
         data.map(({id, title, type, made_with, github}) => {
           return (
-            <section className="project-container">
-              <div className="project-img">
-                <img scr="/" alt="/"></img>
-              </div>
-              <div className="project-info">
-                <div className="project-title">
-                  <h3>{title}</h3>
-                  <h4>{type}</h4>
+              <div className="portfolio-items">
+                <div className="project-img">
+                  <img scr="/" alt="/"></img>
                 </div>
-                <div className="project-links">
-                  <ul className="project-made-with">
-                  { made_with.map((item) =>
-                    <li>{item}</li>
-                  )}
-                  </ul>
-                  <a href={github}><Question /></a>
+                <div className="project-info">
+                  <div className="project-title">
+                    <h3>{title}</h3>
+                    <h4>{type}</h4>
+                  </div>
+                  <div className="project-links">
+                    <ul className="project-made-with">
+                    { made_with.map((item) =>
+                      <li>{item}</li>
+                    )}
+                    </ul>
+                    <a href={github}><Question /></a>
+                  </div>
                 </div>
               </div>
-            </section>
           )
-          })
-        }
+        })
+      }
+      </section>
     </div>
   )
 }
