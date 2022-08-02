50898a9169c9d3eb823daaccf3188eb591f4f8c8
Anja Plemenitas <anja.plemenitas@gmail.com>
2022-08-02 16:47:28 +0100

    form added

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -2,11 +2,34 @@ import React from 'react'
 import './contact.css'
 
 export const Contact = () => {
+  function sendEmail(e) {}
+
   return (
     <div className="contact" id="contact">
       <h1>Contact</h1>
       <hr className="main-line"></hr>
       <h3>anja.plemenitas@gmail.com</h3>
+
+      <form onSubmit={sendEmail}>
+        <label>
+          Subject:
+          <input type="text" name="name" />
+        </label>
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label>
+        <label>
+          Email:
+          <input type="text" name="name" />
+        </label>
+        <label>
+          Message:
+          <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+
       <div className="socials">
         <h2>Socials</h2>
         <hr className="small-line"></hr>
