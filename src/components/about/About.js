b90366b62d2381943d02a5100c84bbaaad3498b5
Anja Plemenitas <anja.plemenitas@gmail.com>
2022-10-07 17:46:33 +0100

    changing about me link

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -9,7 +9,7 @@ export const About = () => {
         Full Stack web <br></br> developer.
       </div>
       <div className="about-more">
-        <a href="https://drive.google.com/file/d/17vvUe8gjdfMtAnXQ-_tP1kJ54UNBnawM/view?usp=sharing">
+        <a href="https://drive.google.com/file/d/1Q6KTvbdCDk7W-IpjR2sWPrDCZCgsclrM/view?usp=sharing">
           More about me
         </a>
       </div>
