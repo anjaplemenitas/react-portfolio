3edc822ae2fd25b1d15232d1fdebceea1facefef
Anja Plemenitas <anja.plemenitas@gmail.com>
2022-10-07 17:10:27 +0100

    Updates

---
+++ precache-manifest.c0f212b926a2221c5b0c6f6d867fc923.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a0ba997435bef3af4ef",
+    "url": "/react-portfolio/static/css/main.38827bd7.chunk.css"
+  },
+  {
+    "revision": "8a0ba997435bef3af4ef",
+    "url": "/react-portfolio/static/js/main.da2a1603.chunk.js"
+  },
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  },
+  {
+    "revision": "370ed964da538fd8ed40",
+    "url": "/react-portfolio/static/js/2.5f9f56bd.chunk.js"
+  },
+  {
+    "revision": "00301474b94780a08b9eebb25d2ed1b1",
+    "url": "/react-portfolio/static/media/oddrecipefirstphoto.00301474.png"
+  },
+  {
+    "revision": "0b27237d04a81927b37bacf56257e855",
+    "url": "/react-portfolio/static/media/allgames.0b27237d.png"
+  },
+  {
+    "revision": "9bdcaef456807ffd27243094dc25f36d",
+    "url": "/react-portfolio/index.html"
+  }
+];
