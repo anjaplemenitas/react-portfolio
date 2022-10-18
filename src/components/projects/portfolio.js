3c2fbcc554ad9e299f034d765224d7e0472f5538
Anja Plemenitas <anja.plemenitas@gmail.com>
2022-10-18 18:32:57 +0100

    instaling tailwind, changed position of the card, adding extra link to project

--- src/components/projects/portfolio.js
+++ src/components/projects/portfolio.js
@@ -9,7 +9,12 @@ const data = [
     title: 'OddRecipe',
     type: 'WebApp',
     made_with: ['Rails', 'Ruby', 'HTML', 'Css', 'JavaScript'],
+    details: [
+      'web app that is helping reduce food waste - inspired by OddBox sustainable company',
+      'coming up with the idea and created a whole visual aspect of it',
+    ],
     github: 'https://github.com/anjaplemenitas/OddRecipe/blob/master/README.md',
+    link: 'https://www.youtube.com/watch?v=f_PJcHAoMx8',
   },
 
   {
@@ -19,8 +24,12 @@ const data = [
     title: 'Games in the Dungeon',
     type: 'Web',
     made_with: ['Rails', 'Ruby', 'HTML', 'Css'],
+    details: [
+      'Rent, buy or create board games on the Dungeon of Games website. My input was in most parts of development, but I worked mostly on the frontend, creating visuals for the website',
+    ],
     github:
       'https://github.com/anjaplemenitas/games_in_the_dungeon/blob/master/README.md',
+    link: '',
   },
 ]
 
